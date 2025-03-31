9e4b95f3fe211453400e581c8c6b43c334e2b2e7
Work <Work@MSI>
2025-03-31 16:02:25 -0500

    Added a couple of things after lecture

--- Templating a CLASS/MyStaticArray.h
+++ Templating a CLASS/MyStaticArray.h
@@ -1,7 +1,17 @@
 #pragma once
 
+
+class IntegerStaticArray
+{
+private:
+	static constexpr int MAX_CAPACITY = 5;
+	int listOfData[MAX_CAPACITY]; //make space for 5 elements of type "T"
+
+
+};
+
 template<typename T> 
-class MyStaticArray
+class GenericStaticArray
 {
 private: 
 	static constexpr int MAX_CAPACITY = 5;
@@ -9,18 +19,21 @@ private:
 
 
 public: 
-	MyStaticArray() = default; 
+	GenericStaticArray() = default; 
 
-	MyStaticArray(const T& theInitialValueOfAll5Elements)
+	GenericStaticArray(const T& theInitialValueOfAll5Elements)
 	{
 		for (int i = 0; i < MAX_CAPACITY; ++i)
 			listOfData[i] = theInitialValueOfAll5Elements;
+
 	}
 
 	void print()
 	{
 		for (int i = 0; i < MAX_CAPACITY; ++i)
 			std::cout << listOfData[i] << " ";
+
+		std::cout << "\n";
 	}
 };
 
--- Templating a CLASS/Templating a CLASS.cpp
+++ Templating a CLASS/Templating a CLASS.cpp
@@ -7,33 +7,19 @@
 
 int main()
 {
-	//int numsArray[5] = { 1, 2, 3,4 ,5 };
 
-	//std::string stringArray[5] =
-	//{
-	//	"Alice",
-	//	"Bob",
-	//	"Carol",
-	//	"Darth",
-	//	"Eve"
-	//};
+/*	std::vector<std::vector<int>> twoDArrayOfNums;*/ //Templates allow this!
+	
+	IntegerStaticArray intArray;
 
 
-/*	std::vector<std::vector<int>> twoDArrayOfNums;*/ 
-	MyStaticArray<int> numsArray(5); 
+	GenericStaticArray<int> numsArray(5); 
 
 	numsArray.print(); 
+
+	GenericStaticArray<std::string> stringArray("asdfasdfsd");
+	stringArray.print(); 
 
 	//MyStaticArray<std::string> stringArray; 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
