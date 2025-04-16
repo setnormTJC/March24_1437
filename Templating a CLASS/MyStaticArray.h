cf7d56c2d8c7a51c741a45bcf073b3b153c9e316
Work <Work@MSI>
2025-04-16 16:08:32 -0500

    Where we ended on April 16.
    Resume with testing `naiveSort` with some arrays of random numbers.
    
    Then move on to `binarySearch` (the RECURSIVE version)

--- Templating a CLASS/MyStaticArray.h
+++ Templating a CLASS/MyStaticArray.h
@@ -4,96 +4,133 @@
 
 #include<cassert>
 
-class IntegerStaticArray
+namespace MySpace
 {
-private:
-	static constexpr int MAX_CAPACITY = 5;
-	int listOfNumbers[MAX_CAPACITY]; //make space for 5 elements of type INT 
 
 
-};
-
-template<typename T, int MAX_CAPACITY> 
-class GenericStaticArray
-{
-private: 
-	//static constexpr int MAX_CAPACITY = 5;
-	T listOfThangs[MAX_CAPACITY]; //make space for 5 elements of type "T"
-
-public: 
-	GenericStaticArray() = default; 
-
-	GenericStaticArray(const T& anInputValue)
+	class IntegerStaticArray
 	{
-		for (int i = 0; i < MAX_CAPACITY; ++i)
-			listOfThangs[i] = anInputValue; 
+	private:
+		static constexpr int MAX_CAPACITY = 5;
+		int listOfNumbers[MAX_CAPACITY]; //make space for 5 elements of type INT 
 
-	}
 
-	GenericStaticArray<T, MAX_CAPACITY> operator + (const GenericStaticArray<T, MAX_CAPACITY>& otherArray)
+	};
+
+	template<typename T, int MAX_CAPACITY> 
+	class GenericStaticArray
 	{
-		GenericStaticArray<T> theResult; 
-		//to be continued...
-		for (int i = 0; i < MAX_CAPACITY; ++i)
-			theResult.listOfThangs[i] = this->listOfThangs[i] + otherArray.listOfThangs[i]; 
+	private: 
+		//static constexpr int MAX_CAPACITY = 5;
+		T listOfThangs[MAX_CAPACITY]; //make space for 5 elements of type "T"
 
+	public: 
+		GenericStaticArray() = default; 
 
-		return theResult; 
-	}
-
-
-	void modifyElementAtIndex(const T& newValueToPutIn, const int theIndexToPutItAt)
-	{
-
-		//assert(theIndexToPutItAt < 0);
-
-		//assert(theIndexToPutItAt < MAX_CAPACITY);
-
-		if (theIndexToPutItAt < 0 ) throw std::exception("index was less than 0!");
-
-		listOfThangs[theIndexToPutItAt] = newValueToPutIn;
-
-		//return; //nuthin'!
-	}
-
-	int linearSearch(const T& thingToSearchFor)
-	{
-		//this is a "O(N) complexity" algorithm -> much more on this next semester 
-		for (int i = 0; i < MAX_CAPACITY; ++i)
+		GenericStaticArray(const T& anInputValue)
 		{
-			if (listOfThangs[i] == thingToSearchFor)
+			for (int i = 0; i < MAX_CAPACITY; ++i)
+				listOfThangs[i] = anInputValue; 
+
+		}
+
+		GenericStaticArray<T, MAX_CAPACITY> operator + (const GenericStaticArray<T, MAX_CAPACITY>& otherArray)
+		{
+			GenericStaticArray<T> theResult; 
+			//to be continued...
+			for (int i = 0; i < MAX_CAPACITY; ++i)
+				theResult.listOfThangs[i] = this->listOfThangs[i] + otherArray.listOfThangs[i]; 
+
+
+			return theResult; 
+		}
+
+		int getIndexOfElement(const T& elementToGetIndexOf)
+		{
+			for (int i = 0; i < MAX_CAPACITY; ++i)
 			{
-				return i; 
+				if (listOfThangs[i] == elementToGetIndexOf)
+				{
+					return i; 
+				}
+			}
+
+			std::cout << elementToGetIndexOf << " was NOT found in the array :(\n";
+			return -1; 
+		}
+
+		void mySwap(const T& firstElement, const T& secondElement)
+		{
+			int indexOfFirstElement = getIndexOfElement(firstElement); //this will be 0 for Darth 
+			int indexOfSecondElement = getIndexOfElement(secondElement); //this will be 1 for Alice (in the current example)
+
+
+			T temporaryCopyOfFirstElement = listOfThangs[indexOfFirstElement];
+
+			listOfThangs[indexOfFirstElement] = listOfThangs[indexOfSecondElement];
+
+			listOfThangs[indexOfSecondElement] = temporaryCopyOfFirstElement;
+
+
+		}
+
+		void modifyElementAtIndex(const T& newValueToPutIn, const int theIndexToPutItAt)
+		{
+		
+			if (theIndexToPutItAt < 0 ) throw std::exception("index was less than 0!");
+
+			listOfThangs[theIndexToPutItAt] = newValueToPutIn;
+
+		}
+
+		int linearSearch(const T& thingToSearchFor)
+		{
+			//this is a "O(N) complexity" algorithm -> much more on this next semester 
+			for (int i = 0; i < MAX_CAPACITY; ++i)
+			{
+				if (listOfThangs[i] == thingToSearchFor)
+				{
+					return i; 
+				}
+			}
+
+			std::cout << "The value " << thingToSearchFor << " is NOT in the array\n";
+			return -1; 
+
+		}
+	
+		void naiveSort()
+		{
+			for (int outer = 0; outer < MAX_CAPACITY - 1; ++outer)
+			{
+				for (int inner = outer + 1; inner < MAX_CAPACITY; ++inner) //previously initialized inner to ONE (1) 
+				{
+					if (listOfThangs[inner] < listOfThangs[outer])
+					{
+						mySwap(listOfThangs[inner], listOfThangs[outer]);
+
+						std::cout << "A SWAP happened! Hooray! Here's the UPDATED array (any key to continue):\n";
+						print(); 
+						std::cin.get(); //requires a single input character before continuing execution
+					}
+				}
 			}
 		}
 
-		std::cout << "The value " << thingToSearchFor << " is NOT in the array\n";
-		return -1; 
+		/*@return the LAST element in the array*/
+		T back()
+		{
+			return listOfThangs[MAX_CAPACITY - 1];
+		}
 
-	}
-	
-	//GenericStaticArray<T> operator - (const GenericStaticArray<T>& otherArray) //prolly NOT a good idea !
+		void print()
+		{
+			for (int i = 0; i < MAX_CAPACITY; ++i)
+				std::cout << listOfThangs[i] << " ";
 
-	void naiveSort()
-	{
-		//to be continued 
-	}
+			std::cout << "\n";
+		}
+	};
 
-	/*@return the LAST element in the array*/
-	T back()
-	{
-		return listOfThangs[MAX_CAPACITY - 1];
-	}
-
-
-	void print()
-	{
-		for (int i = 0; i < MAX_CAPACITY; ++i)
-			std::cout << listOfThangs[i] << " ";
-
-		std::cout << "\n";
-	}
-};
-
-
+}
 
--- Templating a CLASS/main.cpp
+++ Templating a CLASS/main.cpp
@@ -9,50 +9,32 @@
 int main()
 {
 	
-	//int nums[4] = { 1, 2, 3, 4 }; //this is the C (not C++) language way to make a static array
-
-	//std::cout << nums. //doesn't work! nums is not an object of a class! (it's a pointer)
- 
-	//std::cout << nums << "\n";
-
-	//int nums[-1]
-	
-	//std::array<std::string, -2> negativeNames; 
-
-
-	std::array<int, 4> stdArray = { 1, 2, 3, 5 }; 
-
-	
-
-	//std::cout << "Is it 5? " << stdArray.back() << "\n";
-
-/*	std::vector<std::vector<int>> twoDArrayOfNums;*/ //Templates allow this!
-	
-	//IntegerStaticArray intArray;
-
-
-	GenericStaticArray<int, 7> numsArray(5); //a single template parameter (int)
-	//GenericStaticArray<int, 5> OTHERnumsArray(11); 
-
-	const int index = -123; 
-
 	try
 	{
-		numsArray.modifyElementAtIndex(3735635, index); //throws!
+		
+
+		using namespace MySpace; 
+		GenericStaticArray<std::string, 6> names("mouse?");
+		
+		names.modifyElementAtIndex("Darth", 0); 
+		names.modifyElementAtIndex("Alice", 1);
+		names.modifyElementAtIndex("Bob", 2);
+		names.modifyElementAtIndex("Aaaah", 3);
+		names.modifyElementAtIndex("Carol", 4);
+		names.modifyElementAtIndex("Eve", 5);
+	
+		/*testing swap*/
+		//names.mySwap("Darth", "Alice"); 
+
+		names.naiveSort();
+		
+		names.print(); 
 	}
 
 	catch (const std::exception& e)
 	{
 		std::cout << e.what() << "\n";
 	}
-
-
-	numsArray.print(); 
-	std::cout << "The BACK (AKA: last) element in that array is: " << numsArray.back();
-
-	int valueToSearchFor = 12333; 
-
-	std::cout << "Index of " << valueToSearchFor << " is: " << numsArray.linearSearch(valueToSearchFor) << "\n";
 
 
 }
